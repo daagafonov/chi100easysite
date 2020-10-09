3ad67a4899535dc3513abe822463bcfad4d0b446
daagafonov <d2502A1979&%>
2020-10-09 10:34:16 +0300

    erterter

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,17 +5,26 @@ module.exports = {
   },
   extends: [
     "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
+    "@vue/typescript"
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
+    "parser": "@typescript-eslint/parser"
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "class-methods-use-this": 0,
+    "import/prefer-default-export": "off",
+    "eslint global-require": "off",
+    "linebreak-style": [
+        "error",
+        "unix"
+    ],
+    "indent": [
+        "error",
+        4
+    ]
   },
   overrides: [
     {
